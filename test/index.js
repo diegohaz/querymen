4d1f00688b73da9f8bcb1b5abdd2f8aed58a4410
Diego Haz <hazdiego@gmail.com>
2016-04-23 21:19:15 -0300

    Support handlers in top level menquery

--- test/index.js
+++ test/index.js
@@ -2,22 +2,38 @@ import request from 'supertest'
 import express from 'express'
 import test from 'tape'
 import _ from 'lodash'
-import menquery from '../src'
+import * as menquery from '../src'
 import './menquery-param'
 import './menquery-schema'
 
-let app = express()
+test('Menquery handler', (t) => {
+  t.notOk(menquery.parser('testParser'), 'should not get nonexistent parser')
+  t.notOk(menquery.formatter('testFormatter'), 'should not get nonexistent formatter')
+  t.notOk(menquery.validator('testValidator'), 'should not get nonexistent validator')
 
-app.get('/posts', menquery(), (err, req, res, next) => {
-  if (err) {
-    return res.json(err)
-  }
-  next()
-}, (req, res) => {
-  res.status(200).json(_.pick(req, ['filter', 'options', 'test']))
+  menquery.parser('testParser', () => 'test')
+  menquery.formatter('testFormatter', () => 'test')
+  menquery.validator('testValidator', () => ({valid: false}))
+
+  t.ok(menquery.parser('testParser'), 'should get parser')
+  t.ok(menquery.formatter('testFormatter'), 'should get formatter')
+  t.ok(menquery.validator('testValidator'), 'should get validator')
+
+  t.end()
 })
 
 test('Menquery middleware', (t) => {
+  let app = express()
+
+  app.get('/posts', menquery.middleware(), (err, req, res, next) => {
+    if (err) {
+      return res.json(err)
+    }
+    next()
+  }, (req, res) => {
+    res.status(200).json(_.pick(req, ['filter', 'options', 'test']))
+  })
+
   request(app)
     .get('/posts')
     .query({page: 50})
