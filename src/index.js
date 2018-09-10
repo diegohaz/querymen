86c0bce8c801c8b58a2dac08420880d8b2d4cece
Cheun Young Jin <wesias7@gmail.com>
2018-09-10 17:41:56 +0900

    Update index.js
    fixed Schema limit bug

--- src/index.js
+++ src/index.js
@@ -68,18 +68,7 @@ export function validator (name, fn) {
  */
 export function middleware (schema, options) {
   return function (req, res, next) {
-    let _schema
-    // If option near is enable with make a simple clone
-    // In otherwise we make a _.cloneDeep
-    if (schema && schema.options && schema.options.near) {
-      _schema = schema instanceof Schema
-        ? _.clone(schema)
-        : new Schema(schema, options)
-    } else {
-      _schema = schema instanceof Schema
-        ? _.cloneDeep(schema)
-        : new Schema(schema, options)
-    }
+    let _schema = schema instanceof Schema ? _.cloneDeep(schema) : new Schema(schema, options)
 
     _schema.validate(req.query, (err) => {
       if (err) {
